08b4531b740935a410b392184ef4ab8d2476f574
fjlucena <fjlucena@izertis.com>
2022-05-06 12:25:06 +0200

    Initial Commit

---
+++ ISSPredictions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ISSPredictions
+//
+//  Created by Francisco Lucena on 6/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
